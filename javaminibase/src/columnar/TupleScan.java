c7530ca88ecfd5ac0209f553c89993100ecd30a4
poorvikd <64829799+poorvikd@users.noreply.github.com>
2024-03-11 20:45:16 -0700

    Errors fixed

--- javaminibase/src/columnar/TupleScan.java
+++ javaminibase/src/columnar/TupleScan.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 
 import global.*;
 import heap.InvalidTupleSizeException;
-import heap.InvalidTypeException;
 import heap.Scan;
 import heap.Tuple;
 
-public class TupleScan {
+public class TupleScan implements GlobalConst {
     private Columnarfile cf;
     private int ncols;
     AttrType[] attrTypes;
@@ -17,17 +16,17 @@ public class TupleScan {
     short str_size = 25;
     Scan[] sc;
 
-    public TupleScan(Columnarfile f) throws InvalidTupleSizeException, IOException{
+    public TupleScan(Columnarfile f) throws InvalidTupleSizeException, IOException {
         cf = f;
         ncols = f.get_numcols();
         sc = new Scan[ncols];
-        for(int i=1;i<ncols+1;i++){
-            sc[i-1] = f.get_HF(i).openScan();
+        for (int i = 1; i < ncols + 1; i++) {
+            sc[i - 1] = f.get_HF(i).openScan();
         }
         attrTypes = f.get_AttrTypes();
         int str_attr_count = 0;
-        for (int i=0;i<ncols;i++){
-            if (attrTypes[i].attrType==AttrType.attrString){
+        for (int i = 0; i < ncols; i++) {
+            if (attrTypes[i].attrType == AttrType.attrString) {
                 str_attr_count++;
             }
         }
@@ -35,28 +34,28 @@ public class TupleScan {
         Arrays.fill(str_attr_sizes, str_size);
     }
 
-    public void closetuplescan(){
-        for(int i=0;i<ncols;i++){
+    public void closetuplescan() {
+        for (int i = 0; i < ncols; i++) {
             sc[i].closescan();
         }
     }
 
-    Tuple getNext(TID tid) throws InvalidTypeException, InvalidTupleSizeException, IOException{
+    Tuple getNext(TID tid) throws Exception {
         Tuple tpl = new Tuple();
-        tpl.setHdr((short)ncols, attrTypes,str_attr_sizes);
+        tpl.setHdr((short) ncols, attrTypes, str_attr_sizes);
         RID[] rids = new RID[ncols];
         byte[] data = tpl.getTupleByteArray();
         int length = 0;
         int offset = 0;
         int position = 0;
-        for(int i=0;i<ncols;i++){
+        for (int i = 0; i < ncols; i++) {
             RID rid = new RID();
             Tuple temp = sc[i].getNext(rid);
-            if (temp==null)
-            return null;
+            if (temp == null)
+                return null;
             rids[i].copyRid(rid);
-            System.arraycopy(temp.getTupleByteArray(), 0, data, offset,cf.get_attr_size(i));
-            offset+=cf.get_attr_size(i);
+            System.arraycopy(temp.getTupleByteArray(), 0, data, offset, cf.get_attr_size(i));
+            offset += cf.get_attr_size(i);
         }
         position = sc[0].positionOfRecord(rids[0]);
         tid.numRIDs = ncols;
@@ -66,11 +65,11 @@ public class TupleScan {
         return tpl;
     }
 
-    boolean position(TID tid) throws InvalidTupleSizeException, IOException{
-        for(int i=0;i<tid.numRIDs;i++){
+    boolean position(TID tid) throws InvalidTupleSizeException, IOException {
+        for (int i = 0; i < tid.numRIDs; i++) {
             boolean flag = sc[i].position(tid.recordIDs[i]);
             if (!flag)
-            return false;
+                return false;
         }
         return true;
     }
