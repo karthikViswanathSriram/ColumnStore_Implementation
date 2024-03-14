8b4c6aaa89108f1f3a45b06a928444aa3d4635d5
Rithvik Ganesh <rithvikganesh@rithviks-air.dhcp.asu.edu>
2024-03-14 16:08:16 -0700

    Added script file

--- javaminibase/src/script.sh
+++ javaminibase/src/script.sh
@@ -1,5 +1,15 @@
-
 echo "java interfaces/BatchInsert ../sampledata50000.txt mydb colfile 4"
+java interfaces/BatchInsert ../sampledata50000.txt mydb colfile 4
+if [ $1 == "BITMAP" ] || [ $1 == "BTREE" ]; then
+    echo "java interfaces/Index mydb colfile C $1"
+    java interfaces/Index mydb colfile C $1 
+fi
+
+echo "java interfaces/Query mydb colfile A,B,C 'C > 8' 100 ${1}"
 java interfaces/Query mydb colfile A,B,C "C > 8" 100 ${1}
+
+echo "java interfaces/DelQuery mydb colfile A,B,C 'C > 8' 100 $1 "NO PURGE""
 java interfaces/DelQuery mydb colfile A,B,C "C > 8" 100 $1 "NO PURGE"
+
+echo "java interfaces/Query mydb colfile A,B,C 'C > 8' 100 $1"
 java interfaces/Query mydb colfile A,B,C "C > 8" 100 $1
